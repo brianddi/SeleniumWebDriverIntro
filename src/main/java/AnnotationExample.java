df69f5d35ed9d399c984d934b1947607ada5fa7a
Diana <andreea.di.anna@gmail.com>
2023-05-04 19:44:21 +0300

    Initial commit

---
+++ src/main/java/AnnotationExample.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class Class1 {
+
+}
